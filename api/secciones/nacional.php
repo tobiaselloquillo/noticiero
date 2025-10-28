e9c3613a4ad4e1c518507d7abd2001c9d44a30eb
tobiaselloquillo <mfdu16@gmail.com>
2025-10-28 10:43:15 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,9 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "Hablar todo única alta vía sería",
+"titulo" => "Paro de transportistas en el Callao",
 "autor" => "Valentina Balaguer Escamilla",
-"resumen" => "Capaz acciones autoridades cuba texto. Tarde precio bien elementos actual.",
+"resumen" => "Un paro de transportistas ha paralizado el Callao, bloqueando vías y apagando los motores de los buses en señal de protesta por el reciente asesinato de un chofer de transporte. ",
 
 "imagen" => "https://picsum.photos/593/573"
 ];
