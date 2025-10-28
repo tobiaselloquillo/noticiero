defb1643cd2d5292e8669c0ade54c2750d4bb62f
tobiaselloquillo <mfdu16@gmail.com>
2025-10-28 10:41:52 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,9 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "Cámara peor padre",
+"titulo" => "Huracán Melissa alcanza máxima categoría",
 "autor" => "Araceli Cuesta-Puente",
-"resumen" => "Somos dan n quienes carlos acuerdo.",
+"resumen" => "El huracán 'Melissa' ha escalado a la máxima categoría de potencia mientras se dirige hacia la isla de Jamaica.",
 
 "imagen" => "https://picsum.photos/668/459"
 ];
