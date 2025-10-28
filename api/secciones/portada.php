7d0f3fc9f81bf954e17279ba8b24bd01baa12de4
tobiaselloquillo <mfdu16@gmail.com>
2025-10-28 10:40:07 -0500

    Update portada.php

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -4,9 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Casi cabeza personal unos",
+"titulo" => "¿Quien mató al esclavo?",
 "autor" => "Nazario Palma Morell",
-"resumen" => "Pablo más algo tienen dijo comenzó.",
+"resumen" => "La novela La ciudad y los perros de Mario Vargas Llosa no revela claramente quién mató al Esclavo",
 
 "imagen" => "https://picsum.photos/529/541"
 ];
